b41baa5b6d24957c7de3928840e46826a4b72f5c
Jonas Rafael Siqueira Ribeiro <jonas.ribeiro01@fatec.sp.gov.br>
2022-11-20 18:42:33 -0300

    tentativa de upload para heroku

--- app.py
+++ app.py
@@ -1,21 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+cors = CORS(app, resource={r"/*":{"origins": "*"}})
+
+@app.route("/", methods=["GET"])
 def home():
     return render_template("home.html")
 
-@app.route("/sobre-mim", methods=["GET", "POST"])
+@app.route("/sobre-mim", methods=["GET"])
 def sobreMim():
     return render_template("sobre-mim.html")
 
-@app.route("/formacao", methods=["GET", "POST"])
+@app.route("/formacao", methods=["GET"])
 def formacao():
     return render_template("formacao.html")
 
-@app.route("/projetos", methods=["GET", "POST"])
+@app.route("/projetos", methods=["GET"])
 def projetos():
     return render_template("projetos.html")
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
 
 app.run(debug=True)
